39d6adc050838ab7d1405d3b0f2cad4c4c51048b
svgopi <gopinagu2002@gmail.com>
2022-06-05 15:53:27 +0530

    Update

--- Maps/script.js
+++ Maps/script.js
@@ -1,11 +1,10 @@
 var locations = [
-    ['Charminar, Hyderabad - The Charminar lit four minarets constructed in 1591, is a monument and mosque located in Hyderabad, Telangana, India', 17.3616, 78.4747,1],
-    ['Anantagiri Hills - Ananthagiri Hills is located in Vikarabad district, Telangana, India. The water flows from these hills to Usman Sagar is also known as (Gandipet) and Himayathsagar. It is one of the dense forests in Telangana. Ananthagiri Temple is located in this forested area. It is the Birthplace of Musi river, also called as Muchkunda River, which flows through Hyderabad, 5 km from Vikarabad.', 17.3120, 77.8636,14],
-    ['Thousand Pillar Temple - The Thousand Pillar Temple or Rudreswara Swamy Temple is a historic Hindu temple located in the town of Hanamakonda, Telangana State, India. It is dedicated to Lord Shiva, Vishnu and Surya.', 18.0037, 79.5747,13],
-    ['Medak Church - Medak Cathedral at Medak in Telangana, India, is one of the largest churches in India and has been the cathedral church of the Diocese of Medak of the Church of South India since 1947.', 18.053661, 78.267159,12],
-    ['Taj Falaknuma Palace - Falaknuma is a palace in Hyderabad, Telangana, India. It originally belonged to the Paigah family, and was later owned by the Nizam of Hyderabad It is on a hillock and covers a 13-hectare (32-acre) area in Falaknuma, 5 kilometres from Charminar.', 17.3315, 78.4675,11],
-    ['CARE Hospitals, Banjara Hills - CARE Hospitals is a chain of multi-specialty healthcare hospitals with 14 hospitals in 6 cities across 5 states of India.', 17.4126, 78.4503,10],
-    ['Udupi, karnataka - Udupi, otherwise spelt Udipi and also known as Odipu, is a city in the Indian state of Karnataka. Udupi is situated about 55 km north of the educational, commercial and industrial hub of Mangalore and about 422 km west of state capital Bangalore by road.',13.3409, 74.7421,9],
+    ['Charminar, Hyderabad' , 17.3616, 78.4747],
+    ['Anantagiri Hills', 17.3120, 77.8636],
+    ['Thousand Pillar Temple', 18.0037, 79.5747],
+    ['Medak Church', 18.053661, 78.267159],
+    ['Taj Falaknuma Palace', 17.3315, 78.4675],
+	['Udupi, karnataka',13.3409, 74.7421,9],
     ['Jog Falls', 14.2004, 74.7922,8],
     ['Mysuru',12.2958, 76.6394,7],
     ['Bengaluru', 12.9716, 77.5946,6],
@@ -19,8 +18,9 @@ var locations = [
 	['Marina Beach',13.0500, 80.2824,3],
 	['Phoenix Marketcity',12.9958, 77.6964,2],
 	['chennai',13.0827, 80.2707,4]
+	/*
     ['Chhatrapati Shivaji Maharaj Terminus',18.9398, 72.8354,2],
-	['mumbai',19.0760, 72.8777,5],
+	['Mumbai',19.0760, 72.8777,5],
 	['Tarkarli Beach',16.0137, 73.4898,3],
 	['Ajanta Caves',20.5519, 75.7033,1],
 	['Seasons Mall',18.5196, 73.9313,6],
@@ -64,39 +64,86 @@ var locations = [
 	["Valley of Flowers National Park",30.7280,79.6053],
 	["Har Ki Pauri Ghat Haridwar",29.9567,78.1710],
 	["Kedarnath Temple",30.7352,79.0669],
-	["Elephant Falls",25.5361,91.8225]
+	["Elephant Falls",25.5361,91.8225], */
 ];
+
 var map;
-var markers = [];
-
 function initMap(){
-map = new google.maps.Map(document.getElementById('map_canvas'), {
-    zoom: 4,
-    center: new google.maps.LatLng(19.3522816, 69.2045216),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-});
+	var bounds = new google.maps.LatLngBounds();
+	var mapOpt = {
+		mapTypeId: 'hybrid',
+		center: new google.maps.LatLng(23.1827,75.7682)
+	};
+	map = new google.maps.Map(document.getElementById('map_canvas'),mapOpt);
 
+	var infoWindowContent = [
+		[ 
+		'<div style="float:top" class="info_content">' +
+		'<div style="float:right"><img src="img/charminar.jpg" width="200" height="200"></div>' +
+		'<h2> Charminar </h2>' + 
+		'<p> The Charminar lit four minarets constructed in 1591,' + 
+		'is a monument and mosque located in Hyderabad, Telangana, India. </p>' + 
+		'<button style="padding:10px"> <a href="https://www.yatra.com/indian-monuments/hyderabad/charminar" style="text-decoration:none" target="_blank"> Click for more </a> </button>' + '</div>'],
+		
+		['<div style="float:top" class="info_content">' +
+		'<div style="float:right"><img src="img/ananthagiri.jpg" width="280" height="200"></div>' +
+		'<h2> Ananthagiri </h2>' + 
+		'<p> Ananthagiri Hills is located in Vikarabad district, Telangana, India. ' + 
+		'The water flows from these hills to Usman Sagar is also known as (Gandipet) and Himayathsagar. ' + 
+		'It is one of the dense forests in Telangana. Ananthagiri Temple is located in this forested area. ' + 
+		'It is the Birthplace of Musi river, also called as Muchkunda River, which flows through Hyderabad, 5 km from Vikarabad.</p>' + 
+		'<button style="padding:10px"> <a href="https://www.holidify.com/places/ananthagiri-hills/" style="text-decoration:none" target="_blank"> Click for more </a> </button>' + '</div>'],
+
+		['<div style="margin: 25px 10px; float:right"><img src="img/TPT.jpg" width="280" height="250"></div>' +
+		'<div style="float:top" class="info_content">' +
+		'<h2> Thousand Pillar Temple </h2>' + 
+		'<p> The Thousand Pillar Temple or Rudreswara Swamy Temple is a historic Hindu temple' + 
+		'located in the town of Hanamakonda, Telangana State, India. It is dedicated to Lord Shiva, Vishnu and Surya. </p>' +
+		'<button style="padding:10px"> <a href="https://ashokstambh.com/history-and-mystery-of-thousand-pillar-temple/" style="text-decoration:none" target="_blank"> Click for more </a> </button>' + '</div>'],
+
+		['<div style="float:top" class="info_content">' +
+		'<div style="float:right"><img src="img/medak.jpg" width="280" height="250"></div>' +
+		'<h2> Medak Church </h2>' + 
+		'<p> Medak Cathedral at Medak in Telangana, India, is one of the largest churches in India and ' +
+		'has been the cathedral church of the Diocese of Medak of the Church of South India since 1947.. </p>' + 
+		'<button style="padding:10px"> <a href="https://medak.telangana.gov.in/tourist-place/medak-cathedral-church/" style="text-decoration:none" target="_blank"> Click for more </a> </button>' + '</div>'],
+
+		['<div style="float:top" class="info_content">' +
+		'<div style="float:right"><img src="img/FP.jpeg" width="280" height="250"></div>' +
+		'<h2> Falaknuma palace </h2>' + 
+		'<p> Falaknuma is a palace in Hyderabad, Telangana, India. It originally belonged to ' + 
+		'the Paigah family, and was later owned by the Nizam of Hyderabad It is on a hillock and covers a 13-hectare ' +
+		'(32-acre) area in Falaknuma, 5 kilometres from Charminar.</p>' + 
+		'<button style="padding:10px"> <a href="https://www.hyderabadtourism.travel/falaknuma-palace-hyderabad" style="text-decoration:none" target="_blank"> Click for more </a> </button>' + '</div>']
+	];
+
+var infoWindow = new google.maps.InfoWindow(),markers;
 var num = locations.length;
 for (var i = 0; i < num; i++) { 
-
-    markers[i] = new google.maps.Marker({
-    position: {lat:locations[i][1], lng:locations[i][2]},
+	var position = new google.maps.LatLng(locations[i][1],locations[i][2]);
+	bounds.extend(position);
+    markers = new google.maps.Marker({
+    position: position,
     map: map,
-    html: locations[i][0],
-    id: i,
+    title: locations[i][0]
 });
 
-google.maps.event.addListener(markers[i], 'click', function(){
-var infowindow = new google.maps.InfoWindow({
-    id: this.id,
-    content:this.html,
-    position:this.getPosition()
-});
-google.maps.event.addListenerOnce(infowindow, 'closeclick', function(){
-    markers[this.id].setVisible(true);
-});
-    this.setVisible(false);
-    infowindow.open(map);
-});
+google.maps.event.addListener(markers, 'click', (function(markers,i)
+{
+	return function(){
+		infoWindow.setContent(infoWindowContent[i][0]);
+		infoWindow.open(map,markers);
+	}
+	})(markers,i));
+	//map.fitBounds(bounds);
 }
+
+var boundsListener = google.maps.event.addListener((map),'bounds_changed',function(event)
+{
+	this.setZoom(5);
+	google.maps.event.removeListener(boundsListener);
+});
+
 }
+
+google.maps.event.addDomListener(window,'load',initMap);
